182281749eafe9c0f6ab06b74dda62d54e9070a5
Bahrican Akkoyun <bahricanakkoyun@gmail.com>
2019-09-05 23:42:37 +0300

    Add comments to polymorphism example

--- polymorphism/src/com/bahricanakkoyun/mainpackage/Main.java
+++ polymorphism/src/com/bahricanakkoyun/mainpackage/Main.java
@@ -3,15 +3,15 @@ package com.bahricanakkoyun.mainpackage;
 public class Main {
   public static void main(String[] args) {
     // static polymorphism
-    System.out.println(Adder.add(5, 15));
-    System.out.println(Adder.add(5, 15, 10));
-    System.out.println(Adder.add(5.7, 3.6));
-    System.out.println(Adder.add(5.7, 3.6, 7.1));
+    System.out.println(Adder.add(5, 15)); // 20
+    System.out.println(Adder.add(5, 15, 10)); // 30
+    System.out.println(Adder.add(5.7, 3.6)); // 9.3
+    System.out.println(Adder.add(5.7, 3.6, 7.1)); // 16.4
     System.out.println(' ');
 
     // dynamic polymorphism
-    Vehicle vehicle = new Vehicle();
-    Bike bike = new Bike();
+    Vehicle vehicle = new Vehicle(); // Vehicle is running
+    Bike bike = new Bike(); // Bike is running
 
     vehicle.run();
     bike.run();
